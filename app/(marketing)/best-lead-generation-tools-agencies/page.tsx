035d7d88045447a3fd2892f2f63ad7441d2a7c8c
Ismail Courr <ismailcourr@gmail.com>
2026-03-28 09:47:56 +0100

    Remove static pages

--- app/(marketing)/best-lead-generation-tools-agencies/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default function LegacyBestLeadGenerationToolsForAgenciesPage() {
-  permanentRedirect("/blog/best-lead-generation-tools-agencies");
-}
--- app/(marketing)/how-to-find-b2b-leads/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default function LegacyHowToFindB2BLeadsPage() {
-  permanentRedirect("/blog/how-to-find-b2b-leads");
-}
